a88c42ee1a96ecdf8e5afedeff60aacf35278165
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-07 23:31:00 -0500

    Added Basic Variables

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,19 @@
+// console.log("Hello World!!!");
+
+var firstName = "John";
+
+console.log(firstName);
+//String and Variable
+var lastName = "Smith";
+var age = 28; 
+//boolean
+var fullAge = true;
+console.log(fullAge);
+//undefined
+var job;
+console.log(job);
+//now defines
+job = "teacher";
+console.log(job); 
+
+var _3years = 3;
