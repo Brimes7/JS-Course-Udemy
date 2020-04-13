7da04afd0834c5a3b034fa10beb87017bd307444
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-12 21:59:43 -0500

    Finished Variable Mutations.

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,8 +1,6 @@
 /*
 Variable and Data Types
-*/
-
-// console.log("Hello World!!!");
+console.log("Hello World!!!");
 
 var firstName = "John";
 
@@ -20,4 +18,37 @@ console.log(job);
 job = "teacher";
 console.log(job); 
 
-var _3years = 3;
+*/
+
+//Variable mutation and type coercion
+
+// var _3years = 3; 
+
+// var firstName = "John";
+// var age = 28;
+
+// console.log(firstName + " " + age);
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// console.log(firstName = " is a " + age + " year old " + job + ". Is he married?"  + isMarried);
+
+// //Variable mutation
+
+// age = "twenty eight";
+// job = "driver";
+// //Gives an alert not a console
+// // alert(firstName = " is a " + age + " year old " + job + ". Is he married?"  + isMarried);
+
+// var lastName = prompt("What is his last Name?");
+// console.log(firstName + " " +lastName)
+
+
+
+//BASIC OPERATORS.
+
+var yearJohn = 2018-28;
+
+console.log(yearJohn);
