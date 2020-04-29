46521552d1291daaae56a090cb906acf79b4b311
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-29 00:37:50 -0500

    Commenting out things

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -108,16 +108,16 @@ GOOD LUCK 😀
 
 // var isMarried = false;
 
-var firstName = "John";
+// var firstName = "John";
 
-var age = 16;
+// var age = 16;
 
-if(age < 13){
-    console.log(firstName + " is a boy");
-} else if ( age >= 13 && age <= 20){
-    console.log(firstName + " is a teenager.");
-} else if(age >= 20 && age <30) {
-    console.log(firstName =" is a young man");
-} else {
-    console.log(firstName + " is a man");
-}
+// if(age < 13){
+//     console.log(firstName + " is a boy");
+// } else if ( age >= 13 && age <= 20){
+//     console.log(firstName + " is a teenager.");
+// } else if(age >= 20 && age <30) {
+//     console.log(firstName =" is a young man");
+// } else {
+//     console.log(firstName + " is a man");
+// }
