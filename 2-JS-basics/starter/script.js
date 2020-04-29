9b3e87119f81b044c0b5cb28e7d90f869184cf2a
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-28 22:57:42 -0500

    Done

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -116,6 +116,8 @@ if(age < 13){
     console.log(firstName + " is a boy");
 } else if ( age >= 13 && age <= 20){
     console.log(firstName + " is a teenager.");
+} else if(age >= 20 && age <30) {
+    console.log(firstName =" is a young man");
 } else {
     console.log(firstName + " is a man");
 }
